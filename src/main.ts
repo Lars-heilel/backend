0ccddfc4c6305d108165481422c4e0eafd6935cf
Lars Heilel <heuchler228@gmail.com>
2025-05-06 03:28:33 +0300

    init prisma orm and modules user +auth

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,6 @@ async function bootstrap() {
 
     await app.listen(env.get('PORT') ?? 3000);
     Logger.log(` Server on http://localhost:${env.get('PORT')}`);
+    Logger.log(`PgAdmin http://localhost:8080 `);
 }
 bootstrap();
