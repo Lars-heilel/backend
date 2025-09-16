fb359ca6e11212bae93f4fae0d6967dd0f7eb14c
Lars Heilel <heuchler228@gmail.com>
2025-09-16 23:51:11 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap(): Promise<void> {
 
     //Cors
     app.enableCors({
-        origin: true,
+        origin: ['http://localhost:5173', env.get('CLIENT_URL')],
         credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         allowedHeaders: 'Content-Type,Authorization',
